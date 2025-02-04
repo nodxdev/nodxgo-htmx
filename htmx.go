a076667d6b86ce8fa1492352c6f5860c8822b005
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-04 01:07:39 +0000

    Refactor Hx functions to improve documentation clarity and consistency

--- htmx.go
+++ htmx.go
@@ -6,273 +6,252 @@ package htmx
 
 import nodx "github.com/nodxdev/nodxgo"
 
-// Hx is an attribute that renders a hx-[key]="[value]" attribute.
+// Hx renders an hx-[key]="[value]" attribute.
+//
+// https://htmx.org/reference
 func Hx(key string, value string) nodx.Node {
 	return nodx.Attr("hx-"+key, value)
 }
 
-// HxGet is a NodX Node that renders hx-get="[value]" attribute.
+// HxGet renders an hx-get="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-get/
 func HxGet(value string) nodx.Node {
 	return Hx("get", value)
 }
 
-// HxPost is a NodX Node that renders hx-post="[value]" attribute.
+// HxPost renders an hx-post="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-post/
 func HxPost(value string) nodx.Node {
 	return Hx("post", value)
 }
 
-// HxPut is a NodX Node that renders hx-put="[value]" attribute.
+// HxPut renders an hx-put="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-put/
 func HxPut(value string) nodx.Node {
 	return Hx("put", value)
 }
 
-// HxPatch is a NodX Node that renders hx-patch="[value]" attribute.
+// HxPatch renders an hx-patch="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-patch/
 func HxPatch(value string) nodx.Node {
 	return Hx("patch", value)
 }
 
-// HxDelete is a NodX Node that renders hx-delete="[value]" attribute.
+// HxDelete renders an hx-delete="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-delete/
 func HxDelete(value string) nodx.Node {
 	return Hx("delete", value)
 }
 
-// HxOn is a Nodx Node that renders hx-on:[eventName]="[value]" attribute.
+// HxOn renders an hx-on:[eventName]="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-on/
 func HxOn(eventName string, value string) nodx.Node {
 	return Hx("on:"+eventName, value)
 }
 
-// HxPushURL is a NodX Node that renders hx-push-url="[value]" attribute.
+// HxPushURL renders an hx-push-url="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-push-url/
 func HxPushURL(value string) nodx.Node {
 	return Hx("push-url", value)
 }
 
-// HxSelect is a NodX Node that renders hx-select="[value]" attribute.
+// HxSelect renders an hx-select="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-select/
 func HxSelect(value string) nodx.Node {
 	return Hx("select", value)
 }
 
-// HxSelectOOB is a NodX Node that renders hx-select-oob="[value]" attribute.
+// HxSelectOOB renders an hx-select-oob="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-select-oob/
 func HxSelectOOB(value string) nodx.Node {
 	return Hx("select-oob", value)
 }
 
-// HxSwap is a NodX Node that renders hx-swap="[value]" attribute.
+// HxSwap renders an hx-swap="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-swap/
 func HxSwap(value string) nodx.Node {
 	return Hx("swap", value)
 }
 
-// HxSwapOOB is a NodX Node that renders hx-swap-oob="[value]" attribute.
+// HxSwapOOB renders an hx-swap-oob="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-swap-oob/
 func HxSwapOOB(value string) nodx.Node {
 	return Hx("swap-oob", value)
 }
 
-// HxTarget returns a NodX node with the hx-target
-// attribute set to the given value.
+// HxTarget renders an hx-target="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-target/
 func HxTarget(value string) nodx.Node {
 	return Hx("target", value)
 }
 
-// HxTrigger returns a NodX node with the hx-trigger
-// attribute set to the given value.
+// HxTrigger renders an hx-trigger="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-trigger/
 func HxTrigger(value string) nodx.Node {
 	return Hx("trigger", value)
 }
 
-// HxVals returns a NodX node with the hx-vals
-// attribute set to the given value.
+// HxVals renders an hx-vals="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-vals/
 func HxVals(value string) nodx.Node {
 	return Hx("vals", value)
 }
 
-// HxBoost returns a NodX node with the hx-boost
-// attribute set to the given value.
+// HxBoost renders an hx-boost="[value]" attribute.
 //
-// See https://htmx.org/attributes/hx-boost/
+// https://htmx.org/attributes/hx-boost/
 func HxBoost(value string) nodx.Node {
 	return Hx("boost", value)
 }
 
-// HxConfirm returns a NodX node with the hx-confirm
-// attribute set to the given value.
+// HxConfirm renders an hx-confirm="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-confirm/
 func HxConfirm(value string) nodx.Node {
 	return Hx("confirm", value)
 }
 
-// HxDisable returns a NodX node with the hx-disable
-// attribute set to the given value.
+// HxDisable renders an hx-disable="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-disable/
 func HxDisable(value string) nodx.Node {
 	return Hx("disable", value)
 }
 
-// HxDisabledELT returns a NodX node with the hx-disabled-elt
-// attribute set to the given value.
+// HxDisabledELT renders an hx-disabled-elt="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-disabled-elt/
 func HxDisabledELT(value string) nodx.Node {
 	return Hx("disabled-elt", value)
 }
 
-// HxDisinherit returns a NodX node with the hx-disinherit
-// attribute set to the given value.
+// HxDisinherit renders an hx-disinherit="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-disinherit/
 func HxDisinherit(value string) nodx.Node {
 	return Hx("disinherit", value)
 }
 
-// HxEncoding returns a NodX node with the hx-encoding
-// attribute set to the given value.
+// HxEncoding renders an hx-encoding="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-encoding/
 func HxEncoding(value string) nodx.Node {
 	return Hx("encoding", value)
 }
 
-// HxExt returns a NodX node with the hx-ext
-// attribute set to the given value.
+// HxExt renders an hx-ext="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-ext/
 func HxExt(value string) nodx.Node {
 	return Hx("ext", value)
 }
 
-// HxHeaders returns a NodX node with the hx-headers
-// attribute set to the given value.
+// HxHeaders renders an hx-headers="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-headers/
 func HxHeaders(value string) nodx.Node {
 	return Hx("headers", value)
 }
 
-// HxHistory returns a NodX node with the hx-history
-// attribute set to the given value.
+// HxHistory renders an hx-history="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-history/
 func HxHistory(value string) nodx.Node {
 	return Hx("history", value)
 }
 
-// HxHistoryElt returns a NodX node with the hx-history-elt
-// attribute set to the given value.
+// HxHistoryElt renders an hx-history-elt="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-history-elt/
 func HxHistoryElt(value string) nodx.Node {
 	return Hx("history-elt", value)
 }
 
-// HxInclude returns a NodX node with the hx-include
-// attribute set to the given value.
+// HxInclude renders an hx-include="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-include/
 func HxInclude(value string) nodx.Node {
 	return Hx("include", value)
 }
 
-// HxIndicator returns a NodX node with the hx-indicator
-// attribute set to the given value.
+// HxIndicator renders an hx-indicator="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-indicator/
 func HxIndicator(value string) nodx.Node {
 	return Hx("indicator", value)
 }
 
-// HxInherit returns a NodX node with the hx-inherit
-// attribute set to the given value.
+// HxInherit renders an hx-inherit="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-inherit/
 func HxInherit(value string) nodx.Node {
 	return Hx("inherit", value)
 }
 
-// HxParams returns a NodX node with the hx-params
-// attribute set to the given value.
+// HxParams renders an hx-params="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-params/
 func HxParams(value string) nodx.Node {
 	return Hx("params", value)
 }
 
-// HxPreserve returns a NodX node with the hx-preserve
-// attribute set to the given value.
+// HxPreserve renders an hx-preserve="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-preserve/
 func HxPreserve(value string) nodx.Node {
 	return Hx("preserve", value)
 }
 
-// HxPrompt returns a NodX node with the hx-prompt
-// attribute set to the given value.
+// HxPrompt renders an hx-prompt="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-prompt/
 func HxPrompt(value string) nodx.Node {
 	return Hx("prompt", value)
 }
 
-// HxReplaceURL returns a NodX node with the hx-replace-url
-// attribute set to the given value.
+// HxReplaceURL renders an hx-replace-url="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-replace-url/
 func HxReplaceURL(value string) nodx.Node {
 	return Hx("replace-url", value)
 }
 
-// HxRequest returns a NodX node with the hx-request
-// attribute set to the given value.
+// HxRequest renders an hx-request="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-request/
 func HxRequest(value string) nodx.Node {
 	return Hx("request", value)
 }
 
-// HxSync returns a NodX node with the hx-sync
-// attribute set to the given value.
+// HxSync renders an hx-sync="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-sync/
 func HxSync(value string) nodx.Node {
 	return Hx("sync", value)
 }
 
-// HxValidate returns a NodX node with the hx-validate
-// attribute set to the given value.
+// HxValidate renders an hx-validate="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-validate/
 func HxValidate(value string) nodx.Node {
 	return Hx("validate", value)
 }
 
-// HxVars is a NodX Node that renders hx-vars="[value]" attribute.
+// HxVars renders an hx-vars="[value]" attribute.
 //
 // https://htmx.org/attributes/hx-vars/
 func HxVars(value string) nodx.Node {
